259850f77d7bd791320d9f466517cfb83db9dee7
Serkan Sevilgen <ssevilgen@gmail.com>
2013-07-10 11:12:27 +0000

    web.js reads from html file

--- web.js
+++ web.js
@@ -1,6 +1,12 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
+
+
+//var buffer = new Buffer();
+
+var res = fs.readFileSync('index.html','utf8')
+console.log(res);
 
 app.get('/', function(request, response) {
   response.send('Hello World2!');
